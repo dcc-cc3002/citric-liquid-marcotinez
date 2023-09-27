c65fbae63947a17f51bf03e77ce68d8d19c182b0
Marco Martínez <82907965+marcotinez@users.noreply.github.com>
2023-09-26 22:57:20 -0300

    Se fixean las variables de los wildunits
    Se cambia el modo de generar las estadísticas de los wildunits, de manera que no se puedan modificar.

--- src/main/scala/model/entities/wildunit/Seagull.scala
+++ src/main/scala/model/entities/wildunit/Seagull.scala
@@ -9,16 +9,14 @@ import model.entities.GameCharacter
  * in the game. It has a maximum health of 3, an attack of 1, a defense of -1 and an evasion
  * of -1.
  *
- * @param maxHp The maximum health points a player can have. It represents the player's endurance.
- * @param attack The player's capability to deal damage to opponents.
- * @param defense The player's capability to resist or mitigate damage from opponents.
- * @param evasion The player's skill to completely avoid certain attacks.
  *
  * @author [[https://github.com/marcotinez/ Marco Martínez S.]]
  *
  * */
 
-class Seagull(val maxHp: Int = 3,
-              val attack: Int = 1,
-              val defense: Int = -1,
-              val evasion: Int = -1) extends WildUnit with GameCharacter {}
+class Seagull extends WildUnit with GameCharacter {
+  val maxHp: Int = 3
+  val attack: Int = 1
+  val defense: Int = -1
+  val evasion: Int = -1
+}
